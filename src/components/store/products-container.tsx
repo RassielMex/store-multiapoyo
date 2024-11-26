3a6ad64cd3cc5e87c23e2ec112c30b12af243fee
RassielMex <rhcp94.rm@gmail.com>
2024-11-26 08:33:11 -0600

    feat(products): conditional style

--- src/components/store/products-container.tsx
+++ src/components/store/products-container.tsx
@@ -3,7 +3,7 @@ import ProductCard from "./product-card";
 
 export default function ProductsContainer() {
   return (
-    <ProductsGrid>
+    <ProductsGrid $isFavoritesVisible={false}>
       <ProductCard />
       <ProductCard />
       <ProductCard />
@@ -16,27 +16,32 @@ export default function ProductsContainer() {
   );
 }
 
-const ProductsGrid = styled.div`
+const ProductsGrid = styled.div<{ $isFavoritesVisible: boolean }>`
   position: relative;
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(1, minmax(0, 1fr));
+  grid-template-columns: repeat(1, minmax(min-content, 1fr));
   grid-auto-flow: dense;
   gap: 1rem;
   justify-self: center;
+  justify-items: center;
 
   @media (min-width: 650px) {
-    grid-template-columns: repeat(2, minmax(0, 1fr));
+    grid-template-columns: repeat(2, minmax(min-content, 1fr));
   }
 
   @media (min-width: 900px) {
-    grid-template-columns: repeat(3, minmax(0, 1fr));
+    grid-template-columns: repeat(3, minmax(min-content, 1fr));
   }
 
   @media (min-width: 1224px) {
-    grid-template-columns: repeat(4, minmax(0, 1fr));
+    grid-template-columns: repeat(4, minmax(min-content, 1fr));
   }
+
   @media (min-width: 1600px) {
-    grid-template-columns: repeat(5, minmax(0, 1fr));
+    grid-template-columns: ${(props) =>
+      props.$isFavoritesVisible
+        ? "repeat(4, minmax(min-content, 1fr))"
+        : "repeat(5, minmax(min-content, 1fr))"};
   }
 `;
